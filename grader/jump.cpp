13a493c131cf4968d07b10746c6c10ef3bd4b852
kuikaopong <kuikaopong.2002@gmail.com>
2022-11-04 15:06:33 +0700

    save

--- grader/jump.cpp
+++ grader/jump.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int n_nodes;
 int n_distance;
-const int MAX_NODES = 1010;
+const int MAX_NODES = 100010;
 vector<int> adjacency[MAX_NODES];
 int degree[MAX_NODES];
 
@@ -20,7 +20,7 @@ void print_all_nodes(int n_nodes)
 {
     for (int node = 0; node < n_nodes + 2; node++)
     {
-        cout << "Node: " << node << ", (x, y) = (" << all_nodes[node].x << ", " << all_nodes[node].y << ')' << endl;
+        //cout << "Node: " << node << ", (x, y) = (" << all_nodes[node].x << ", " << all_nodes[node].y << ')' << endl;
     }
 }
 void read_input()
@@ -41,7 +41,7 @@ int find_distance(Node first_node, Node last_node)
     int x_difference, y_difference;
     x_difference = abs(first_node.x - last_node.x);
     y_difference = abs(first_node.y - last_node.y);
-    return sqrt(pow(x_difference, 2) + pow(y_difference, 2));
+    return pow(x_difference, 2) + pow(y_difference, 2);
 }
 void create_adjacency(Node *all_nodes, int n_nodes)
 {
@@ -49,7 +49,7 @@ void create_adjacency(Node *all_nodes, int n_nodes)
     {
         for (int next_node = node + 1; next_node < n_nodes + 2; next_node++)
         {
-            if (find_distance(all_nodes[node], all_nodes[next_node]) <= n_distance)
+            if (find_distance(all_nodes[node], all_nodes[next_node]) <= n_distance*n_distance)
             {
                 adjacency[node].push_back(next_node);
                 adjacency[next_node].push_back(node);
@@ -63,18 +63,17 @@ void print_adjacency(vector<int> adjacency[], int n_nodes)
 {
     for (int node = 0; node < n_nodes + 2; node++)
     {
-        cout << "adjacency: " << node << ", contain: [";
+        //cout << "adjacency: " << node << ", contain: [";
         for (int each_node = 0; each_node < adjacency[node].size(); each_node++)
         {
             cout << adjacency[node][each_node] << ", ";
         }
-        cout << ']' << endl;
+        //cout << ']' << endl;
     }
 }
 
 int layer[MAX_NODES];
 bool seen[MAX_NODES];
-
 void bfs(int s, int *counter)
 {
     vector<int> current_layer;
@@ -83,14 +82,13 @@ void bfs(int s, int *counter)
     current_layer.push_back(s);
     layer[s] = 0;
     seen[s] = true;
-
     while (true)
     {
         // look at all the nodes in current layer
         for (auto i = current_layer.begin(); i != current_layer.end(); i++)
         {
             int u = *i;
-            // cout << "visite: " << u << ",layer: " << layer[u] << endl;
+            //cout << "visite: " << u << ",layer: " << layer[u] << endl;
             *counter = layer[u];
 
             // iterate over nodes in adjacency[u]
@@ -130,19 +128,23 @@ void init_bfs()
 int main()
 {
     read_input();
-    // print_all_nodes(n_nodes);
+    //print_all_nodes(n_nodes);
     create_adjacency(all_nodes, n_nodes);
-    // print_adjacency(adjacency, n_nodes);
+    //print_adjacency(adjacency, n_nodes);
     init_bfs();
+  
     int counter = 0;
-    for (int i = 0; i < n_nodes; i++)
-    {
-        if (!seen[i])
-        {
-            bfs(i, &counter);
-        }
+    bfs(0,&counter);
+    int level = layer[n_nodes + 1];
+    
+    
+    if(seen[n_nodes+1]){
+      cout << level << endl;
     }
-    cout << counter;
+    else {
+      cout << "-1"<<endl;
+    }
+
 
     return 0;
 }
