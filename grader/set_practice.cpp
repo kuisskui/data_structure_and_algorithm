7bfb1180989f7ebeb0f2eec710e664ea7d5cd480
kuisskui <kuikaopong.2002@gmail.com>
2022-11-21 00:07:12 +0700

    save set_practice

--- grader/set_practice.cpp
+++ grader/set_practice.cpp
@@ -3,20 +3,63 @@
 
 using namespace std;
 
+void display(set<int> s)
+{
+    for (auto itr : s)
+    {
+        cout << itr << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
-    set<int> x, y;
-    int n, m, input;
+    int n, m, input, min;
     cin >> n >> m;
-    for (int i = 0; i < n; i++)
+    set<int> x;
+    int array[100010];
+
+    if (n < m)
     {
-        cin >> input;
-        x.insert(input);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> array[i];
+        }
+        for (int i = 0; i < m; i++)
+        {
+            cin >> input;
+            x.insert(input);
+        }
+        min = n;
     }
-    for (int i = 0; i < m; i++)
+    else
     {
-        cin >> input;
-        y.insert(input);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> input;
+            x.insert(input);
+        }
+        for (int i = 0; i < m; i++)
+        {
+            cin >> array[i];
+        }
+        min = m;
     }
-    
+    set<int>::iterator upper, lower;
+    for (int i = 0; i < min; i++)
+    {
+        upper = x.upper_bound(array[i]);
+        lower = x.lower_bound(array[i]);
+        cout << "i: " << array[i] << endl;
+        cout << "upper: "<< *upper << endl;
+        cout << "lower: "<< *lower << endl;
+        if (abs(array[i] - *upper) < abs(array[i] - *lower))
+        {
+            cout << *upper << endl;
+        }
+        else
+        {
+            cout << *lower << endl;
+        }
+    }
 }
--- grader/work_space.cpp
+++ grader/work_space.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <set>
 using namespace std;
-
 int main()
 {
-  char *text = (char*)malloc(sizeof(char)*10);
-  cout << *text;
-  cin >> *text;
-  text++;
-  cout << *text;
+  std::set<int> Set;
+  std::set<int>::iterator one, end;
+  for (int i = 1; i < 10; i++)
+    Set.insert(i * 10);
+  int value = 25;
+  one = Set.lower_bound(100);
+  end = Set.upper_bound(100);
+  cout << "Elements are: ";
+  for (std::set<int>::iterator i = Set.begin(); i != Set.end(); ++i)
+    std::cout << ' ' << *i;
+  std::cout << ' ' << endl;
+  cout << "lower: " << *one << endl;
+  cout << "upper: " << *end << endl;
+  return 0;
 }
